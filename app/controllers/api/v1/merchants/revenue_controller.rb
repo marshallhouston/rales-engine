72225ffcbe7565000f185f4b4842cfa6589e5f9c
Marshall Houston <marshall.houston@gmail.com>
2017-12-03 19:22:25 -0700

    add serializer for total revenue by date

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::Merchants::RevenueController < ApplicationController
   before_action :format_date
 
   def index
-    render json: { total_revenue: Merchant.total_revenue(params["date"]).to_s }
+    render json: Merchant.total_revenue(params["date"]).to_s, serializer: MerchantTotalRevenueByDateSerializer
   end
-  
+
   private
 
   def format_date
